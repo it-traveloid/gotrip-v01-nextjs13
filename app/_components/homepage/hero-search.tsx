d70d8b3a9942dbbb07ca728f5c42bf284cb7db7b
m-amirazmi <60350614+m-amirazmi@users.noreply.github.com>
2022-11-23 23:11:50 +0800

    location search popup

--- app/_components/homepage/hero-search.tsx
+++ app/_components/homepage/hero-search.tsx
@@ -1,10 +1,60 @@
+'use client';
+import React, { useEffect, useState } from 'react';
+import { locations } from '../../../utils/locations';
+
 export default function HeroSearch() {
+	const [open, setOpen] = useState('');
+	const [selectedLocation, setSelectedLocation] = useState({ id: 0, region: '', state: '' });
+
+	useEffect(() => {
+		if (open) document.addEventListener('click', (e: any) => e.target.name !== open && setOpen(''));
+	}, [open]);
+
+	const handleOpen = (e: React.MouseEvent<HTMLButtonElement>) => {
+		if (open === e.currentTarget.name) setOpen('');
+		else setOpen(e.currentTarget.name);
+	};
+
+	const handleSelectedLocation = (e: React.MouseEvent<HTMLButtonElement>) => {
+		const findLocation = locations.find((l) => l.id.toString() === e.currentTarget.id);
+		if (!!findLocation) setSelectedLocation(findLocation);
+	};
+
 	return (
-		<div className="absolute -z-10 bottom-[13%] hidden w-full text-base md:block">
+		<div className="absolute bottom-[13%] hidden w-full text-base md:block">
 			<div className="flex w-11/12 rounded bg-white p-3 text-text-color-1 shadow-xl lg:w-8/12">
-				<div className="w-1/3 border-r pl-3">
+				<div className="relative w-1/3 border-r pl-3">
 					<div>Location</div>
-					<input className="mt-1 w-full text-text-color-2 focus:outline-none" type="text" placeholder="Where are you going?" />
+					<button name="location" onClick={handleOpen} className="text-left cursor-pointer mt-1 w-full text-text-color-2 focus:outline-none">
+						{selectedLocation.id ? selectedLocation.region + ' - ' + selectedLocation.state : 'Where are you going?'}
+					</button>
+					{open === 'location' && (
+						<div className="absolute bottom-20 -left-3 bg-white p-3 w-full rounded text-sm shadow-xl h-[300px] overflow-y-scroll scrollbar-hide">
+							{locations.map((l) => {
+								return (
+									<button
+										id={l.id.toString()}
+										className={`text-left w-full cursor-pointer rounded hover:bg-primary-1 hover:bg-opacity-10 hover:text-primary-1 relative flex flex-row items-center gap-3 p-2 ${
+											selectedLocation.id === l.id ? 'bg-primary-1 bg-opacity-10 text-primary-1' : ''
+										}`}
+										onClick={handleSelectedLocation}
+										key={l.id}
+									>
+										<div className="text-text-color-2">
+											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+												<path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
+												<path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+											</svg>
+										</div>
+										<div>
+											<div className="font-medium">{l.region}</div>
+											<div className="text-text-color-2">{l.state}</div>
+										</div>
+									</button>
+								);
+							})}
+						</div>
+					)}
 				</div>
 				<div className="w-1/3 border-r pl-6">
 					<div>Check in - Check out</div>
---
+++ utils/locations.ts
@@ -0,0 +1,27 @@
+export const locations = [
+	{
+		id: 1,
+		region: 'Ampang',
+		state: 'Selangor',
+	},
+	{
+		id: 2,
+		region: 'Gombak',
+		state: 'Selangor',
+	},
+	{
+		id: 3,
+		region: 'KLCC',
+		state: 'Kuala Lumpur',
+	},
+	{
+		id: 4,
+		region: 'Petaling Jaya',
+		state: 'Selangor',
+	},
+	{
+		id: 5,
+		region: 'Damansara',
+		state: 'Selangor',
+	},
+];
