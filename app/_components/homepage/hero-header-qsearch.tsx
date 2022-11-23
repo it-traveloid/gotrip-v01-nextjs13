f8cc54d7fa4ee414b8e1a8924eacb1360dfa9eb2
m-amirazmi <60350614+m-amirazmi@users.noreply.github.com>
2022-11-23 14:51:48 +0800

    fix the bg image

--- app/_components/homepage/hero-header-qsearch.tsx
+++ app/_components/homepage/hero-header-qsearch.tsx
@@ -8,7 +8,11 @@ export default function HeroHeaderQSearch() {
 					<path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
 				</svg>
 			</div>
-			<input className="bg-transparent py-4 pr-4 pl-10 w-full focus:outline-none focus:bg-primary-4 focus:bg-opacity-25 rounded" type="text" placeholder="Destination..." />
+			<input
+				className="bg-transparent py-4 pr-4 pl-10 w-full placeholder:text-white placeholder:text-opacity-70 focus:outline-none focus:bg-primary-4 focus:bg-opacity-25 rounded"
+				type="text"
+				placeholder="Destination..."
+			/>
 		</div>
 	);
 }
